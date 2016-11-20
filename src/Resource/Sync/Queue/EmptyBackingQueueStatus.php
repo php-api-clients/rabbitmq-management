5c1a2bad4fe37906df6dfd1ff0fde2006902ed61
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-20 15:46:02 +0100

    Sync empty backing queue status

---
+++ src/Resource/Sync/Queue/EmptyBackingQueueStatus.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Client\RabbitMQ\Management\Resource\Sync\Queue;
+
+use ApiClients\Client\RabbitMQ\Management\Resource\Queue\EmptyBackingQueueStatus as BaseEmptyBackingQueueStatus;
+
+class EmptyBackingQueueStatus extends BaseEmptyBackingQueueStatus
+{
+}
