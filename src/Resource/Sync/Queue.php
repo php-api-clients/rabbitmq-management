e4ad5cde200625cc52c2be8b029a23e1b857f225
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-20 15:44:54 +0100

    Sync Queue

---
+++ src/Resource/Sync/Queue.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Client\RabbitMQ\Management\Resource\Sync;
+
+use ApiClients\Client\RabbitMQ\Management\Resource\Queue as BaseQueue;
+
+class Queue extends BaseQueue
+{
+    public function refresh() : Queue
+    {
+        return $this->wait($this->callAsync('refresh'));
+    }
+}
