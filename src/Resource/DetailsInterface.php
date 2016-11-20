80c9cc562fd28c54a13098fea8b39c72203efa38
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-20 15:44:24 +0100

    Details interface

---
+++ src/Resource/DetailsInterface.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Client\RabbitMQ\Management\Resource;
+
+use ApiClients\Foundation\Resource\ResourceInterface;
+
+interface DetailsInterface extends ResourceInterface
+{
+    /**
+     * @return float
+     */
+    public function rate() : float;
+}
