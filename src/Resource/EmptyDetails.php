fb4ac6dde1c6629be24cf337fdc3f7e500a707eb
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-20 15:44:10 +0100

    Empty details

---
+++ src/Resource/EmptyDetails.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Client\RabbitMQ\Management\Resource;
+
+use ApiClients\Foundation\Resource\EmptyResourceInterface;
+
+abstract class EmptyDetails implements DetailsInterface, EmptyResourceInterface
+{
+    /**
+     * @return float
+     */
+    public function rate() : float
+    {
+        return null;
+    }
+}
