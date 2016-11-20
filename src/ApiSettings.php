ee40b2089925821677c1330ee946c9147bd892dd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-20 15:36:07 +0100

    ApiSettings

---
+++ src/ApiSettings.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace ApiClients\Client\RabbitMQ\Management;
+
+use ApiClients\Foundation\Hydrator\Options as HydratorOptions;
+use ApiClients\Foundation\Options;
+use ApiClients\Foundation\Transport\Middleware\JsonDecodeMiddleware;
+use ApiClients\Foundation\Transport\Options as TransportOptions;
+use ApiClients\Foundation\Transport\UserAgentStrategies;
+
+final class ApiSettings
+{
+    const NAMESPACE = 'ApiClients\\Client\\RabbitMQ\\Management\\Resource';
+
+    const TRANSPORT_OPTIONS = [
+        Options::HYDRATOR_OPTIONS => [
+            HydratorOptions::NAMESPACE => self::NAMESPACE,
+            HydratorOptions::NAMESPACE_DIR => __DIR__ . DIRECTORY_SEPARATOR . 'Resource' . DIRECTORY_SEPARATOR,
+        ],
+        Options::TRANSPORT_OPTIONS => [
+            TransportOptions::HEADERS => [
+                'Content-Type' => 'application/json',
+            ],
+            TransportOptions::MIDDLEWARE => [
+                JsonDecodeMiddleware::class,
+            ],
+            TransportOptions::USER_AGENT_STRATEGY => UserAgentStrategies::PACKAGE_VERSION,
+            TransportOptions::PACKAGE => 'api-clients/rabbitmq-management',
+        ],
+    ];
+
+    public static function getOptions(string $baseUrl, string $username, string $password, string $namespaceSuffix): array
+    {
+        $url = parse_url($baseUrl);
+        $auth = 'Basic ' . base64_encode($username . ':' . $password);
+
+        $options = self::TRANSPORT_OPTIONS;
+        $transportOptions = $options[Options::TRANSPORT_OPTIONS];
+        $transportOptions[TransportOptions::SCHEMA] = $url['scheme'];
+        $transportOptions[TransportOptions::HOST] = $url['host'] . ':' . $url['port'];
+        $transportOptions[TransportOptions::PORT] = $url['port'];
+        $transportOptions[TransportOptions::PATH] = $url['path'] . 'api/';
+        $transportOptions[TransportOptions::HEADERS]['Authorization'] = $auth;
+        $options[Options::TRANSPORT_OPTIONS] = $transportOptions;
+
+        $options[Options::HYDRATOR_OPTIONS][HydratorOptions::NAMESPACE_SUFFIX] = $namespaceSuffix;
+
+        return $options;
+    }
+}
